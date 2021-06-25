6d2a9aacb9d4c0eb14eeed3603ad491620dc6938
chobhe <68486069+chobhe@users.noreply.github.com>
2021-06-25 15:36:31 -0500

    Add files via upload

--- emailCapability.py
+++ emailCapability.py
@@ -1,19 +1,22 @@
 import smtplib, ssl
 
-port = 465  # For SSL
-password = 'CC3ngLabs!'
 
-sender_email = "automationproductsafety@gmail.com"
-receiver_email = "he.Charlie@yahoo.com"
-message = """\
-Subject: Thermal Test Automation
+def SendEmail(receiver_email):
+    port = 465  # For SSL
+    password = 'CC3ngLabs!'
 
-Your thermal test automation is complete, come back to the automation station!"""
+    sender_email = "automationproductsafety@gmail.com"
+    message = """\
+    Subject: Thermal Test Automation
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+    Your thermal test automation is complete, come back to stop the test and check your file for data"""
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, password)
-    # TODO: Send email here
-    server.sendmail(sender_email, receiver_email, message)
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        # TODO: Send email here
+        server.sendmail(sender_email, receiver_email, message)
+
+    return 'Done'
